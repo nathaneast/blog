43bfb79b80feb7790e438d4e960ca13cdde5322f
nathaneast <47707076+nathaneast@users.noreply.github.com>
2020-09-09 16:51:27 +0900

    add infinity scroll

--- client/src/routes/nomalRoute/PostCardList.js
+++ client/src/routes/nomalRoute/PostCardList.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { POST_LOADING_REQUEST } from "../../redux/types";
 import { Helmet } from "react-helmet";
-import { Row } from "reactstrap";
+import { Row, Alert } from "reactstrap";
 import { GrowingSpinner } from "../../components/spinner/Spinner";
 import PostCardOne from "../../components/post/PostCardOne";
 import Category from "../../components/post/Category";
@@ -17,6 +17,44 @@ const PostCardList = () => {
     dispatch({ type: POST_LOADING_REQUEST, payload: 0 });
   }, [dispatch]);
 
+  // infinity scroll
+  ///////////////////////////////////
+  const skipNumberRef = useRef(0);
+  const postCountRef = useRef(0);
+  const endMsg = useRef(false);
+
+  postCountRef.current = postCount - 6;
+
+  const observer = useRef();
+
+  const lastPostElementRef = useCallback((node) => {
+    if (loading) return;
+
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        let remainPostCount = postCountRef.current - skipNumberRef.current;
+        if (remainPostCount >= 0) {
+          dispatch({
+            type: POST_LOADING_REQUEST,
+            payload: skipNumberRef.current + 6,
+          });
+          skipNumberRef.current += 6;
+        } else {
+          endMsg.current = true;
+        }
+      }
+      console.log(entries);
+    });
+
+    if (observer.current) observer.current.disconnect();
+
+    if (node) {
+      console.log(node);
+      observer.current.observe(node);
+    }
+  });
+  ///////////////////////////////////
+
   return (
     <>
       <Helmet title="Home" />
@@ -24,6 +62,18 @@ const PostCardList = () => {
         <Category posts={categoryFindResult} />
       </Row>
       <Row>{posts ? <PostCardOne posts={posts} /> : GrowingSpinner}</Row>
+      <div ref={lastPostElementRef}>{loading && GrowingSpinner}</div>
+      {loading ? (
+        ""
+      ) : endMsg ? (
+        <div>
+          <Alert color="danger" className="text-center font-weight-bolder">
+            더 이상의 포스트는 없습니다
+          </Alert>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 };
